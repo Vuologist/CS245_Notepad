490ad45e9cb2d5759a2625017fd263864df49980
Anthony Vu <anthony.vu2142@gmail.com>
2018-02-22 15:41:38 -0800

    added file

---
+++ Notepad.java
@@ -0,0 +1,15 @@
+public class Notepad {
+
+    /*
+        classes:
+        Notepad() - main
+        menuBar
+
+
+
+    */
+    public static void main (String args[]){
+        System.out.println("notepad");
+    }
+
+}
